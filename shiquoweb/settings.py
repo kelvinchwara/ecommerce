89871603e2283679957766bcccc66f8d6c0f869d
kelvinchwara <kelvi20sagini@gmail.com>
2025-03-26 19:51:32 +0300

    ke cgY

--- shiquoweb/settings.py
+++ shiquoweb/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -83,9 +84,10 @@ WSGI_APPLICATION = 'shiquoweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/var/db/db.sqlite3',  # Ensure this matches your mount point.
     }
 }
+
 
 
 # Password validation
