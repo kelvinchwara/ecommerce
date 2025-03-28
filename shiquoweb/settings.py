0ac124936e3e9d65a33fdae8d2102149e07ae411
kelvinchwara <kelvi20sagini@gmail.com>
2025-03-28 14:51:51 +0300

    Initial commit

--- shiquoweb/settings.py
+++ shiquoweb/settings.py
@@ -53,10 +53,11 @@ WSGI_APPLICATION = 'shiquoweb.wsgi.application'
 
 # Database configuration
 
-
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Adjust the path as needed
+    }
 }
 
 
